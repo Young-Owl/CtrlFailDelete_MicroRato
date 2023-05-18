85be7634aeee7b9dc444f4f366a7e8c4f728f6c8
Young-Owl <goncalorodrigues01@gmail.com>
2023-05-18 14:17:07 +0100

    Updated code; New interation

--- src/main.cpp
+++ src/main.cpp
@@ -2,61 +2,63 @@
   *  2022/2023
   *  UA - Universidade de Aveiro
   *  DETI - Departamento de Electrónica, Telecomunicações e Informática
-  *  Gonçalo Rodrigues; Marco; João Tavares;
+  *  Gonçalo Rodrigues; Marco Santos; João Tavares;
 */
 
 /* Every generation = Add a new line: */
 /*          CTRL+FAIL+DELETE          */ // Initial Code for Tests
 /*          CTRL+FAIL+DELETE          */ // Iteration 0 - Basic sensor testing
 /*          CTRL+FAIL+DELETE          */ // Iteration 1 - Sensors not working; Added 2 sensors + Color Sensor
+/*          CTRL+FAIL+DELETE          */ // Iteration 2 - Sensors were actually working, Cathode and Anode were inverted! :D
 
 #include <Arduino.h>
 #include <Wire.h>
 #include <MPU6050_tockn.h>
+#include <EasyButton.h>
+
+/* Debug purposes only, remove comment to see the outputs */
+#define DEBUG
+
 
 /* PINs SCL and SDA (I2C Protocol) will be used for the GY-521 Module (Gyroscope)*/
+MPU6050 mpu6050(Wire);
 
-/* Defining PINs for the L298N Module */
+/* Defining PINs for the L298N Module 
+ * Clockwise = Wheel pushes the robot forward;
+ * Anti-Clockwise = Wheel pushes the robot backwards;     */ 
 #define M1_EN     2   /**< Motor 1 Speed Control. (PWM)   */
 #define M1_CLOCK  53  /**< Motor 1 Clockwise Control.     */
-#define M1_ACLOCK 3   /**< Motor 1 Anti-Clockwise Control.*/
+#define M1_ACLOCK 52   /**< Motor 1 Anti-Clockwise Control.*/
 #define M2_EN     3   /**< Motor 2 Speed Control. (PWM)   */
 #define M2_CLOCK  51  /**< Motor 2 Clockwise Control.     */
-#define M2_ACLOCK 5   /**< Motor 2 Anti-Clockwise Control.*/
+#define M2_ACLOCK 50   /**< Motor 2 Anti-Clockwise Control.*/
 
 /* Defining PINs for the 2 side IR sensors */
-#define IRL_EN 4      /**< Right IR Sensor Led Enable */
-#define IRL A0        /**< Right IR Sensor Analog Pin */
-#define IRR_EN 5      /**< Left IR Sensor Led Enable  */
-#define IRR A1        /**< Left IR Sensor Analog Pin  */
-
-/* Defining PINs for the front color sensor */
-#define S0 6          /**< Color Sensor S0 Pin */
-#define S1 7          /**< Color Sensor S1 Pin */
-#define S2 8          /**< Color Sensor S2 Pin */
-#define S3 9          /**< Color Sensor S3 Pin */
-#define OUT 10        /**< Color Sensor OUT Pin */
+#define IR_EN 24       /**< Global IR Sensor enable */
+#define IRFR A2       /**< Front Right IR Sensor Analog Pin */
+#define IRFL A3       /**< Front Left IR Sensor Analog Pin  */
+#define IRR A5        /**< Right IR Sensor Analog Pin */
+#define IRL A4        /**< Left IR Sensor Analog Pin  */
 
 /* Defining PINs for the START and STOP Buttons */
-#define START 11      /**< Start Button Pin */
-#define STOP 12       /**< Stop Button Pin  */
+#define START 28      /**< Start Button Pin */
+#define STOP 29       /**< Stop Button Pin  */
 
-/*
-volatile int time = 0;
-volatile int lastPulse = 0;
-volatile int pinCurrentState = 0;
-volatile int pinLastState = 0;
-volatile bool direction = false;  /**< false = Anti-Clockwise, true = Clockwise */
-
-int R, G, B;              /**< Color Sensor RGB Values */
-int DATA;                 /**< Color Sensor Data Value */
-bool LINE;                /**< Color Sensor Line Detection */
-int AVG_IRL, AVG_IRR;     /**< Average IR Sensor Value */
-int IRR_LDATA ,IRL_LDATA; /**< IR Sensor Last Values */
-int IRR_DATA, IRL_DATA;   /**< IR Sensor Storage Variables */
+int AVG_IRL, AVG_IRR, AVG_IRFL, AVG_IRFR;         /**< Average IR Sensor Value */
+int IRR_LDATA ,IRL_LDATA, IRFR_LDATA, IRFL_LDATA; /**< IR Sensor Last Values */
+int IRR_DATA, IRL_DATA, IRFR_DATA, IRFL_DATA;     /**< IR Sensor Storage Variables */
+int M1_SPEED = 0, M2_SPEED = 0;                   /**< Motor Speed Variables */
+EasyButton startButton(START, 30, false, false);                      /**< Start Button Object */
+EasyButton stopButton(STOP, 30, false, false);                        /**< Stop Button Object */
+int startState = 0, stopState = 0;                /**< Button State Variable */
+int lstartState = 0, lstopState = 0;              /**< Last Button State Variable */
 
 
 void goStraight(){
+  M1_SPEED = 225;
+  M2_SPEED = 255;
+  analogWrite(M1_EN,M1_SPEED);
+  analogWrite(M2_EN,M2_SPEED);
   digitalWrite(M1_CLOCK,HIGH);
   digitalWrite(M2_CLOCK,HIGH);
   digitalWrite(M1_ACLOCK,LOW);
@@ -64,6 +66,10 @@ void goStraight(){
 }
 
 void goRight(){
+  M1_SPEED = 225;
+  M2_SPEED = 255;
+  analogWrite(M1_EN,M1_SPEED);
+  analogWrite(M2_EN,M2_SPEED);
   digitalWrite(M1_CLOCK,HIGH);
   digitalWrite(M2_CLOCK,LOW);
   digitalWrite(M1_ACLOCK,LOW);
@@ -71,59 +77,52 @@ void goRight(){
 }
 
 void goLeft(){
+  M1_SPEED = 225;
+  M2_SPEED = 255;
+  analogWrite(M1_EN,M1_SPEED);
+  analogWrite(M2_EN,M2_SPEED);
   digitalWrite(M1_CLOCK,LOW);
   digitalWrite(M2_CLOCK,HIGH);
   digitalWrite(M1_ACLOCK,HIGH);
   digitalWrite(M2_ACLOCK,LOW);
 }
 
-/**
- * Average delay time is around 30 - 40 ms
- */
-void readColorSensor(){
-  /* S2/S3 Levels define which set of photodiodes we are using 
-   * LOW/LOW is for RED 
-   * LOW/HIGH is for BLUE 
-   * HIGH/HIGH is for GREEN 
-   */
-
-  digitalWrite(S2,LOW);        
-  digitalWrite(S3,LOW);
-  Serial.print("Red value= "); 
-  DATA = pulseIn(OUT,LOW);
-  delay(10);
-  R = DATA;
-  Serial.print(R); 
-
-  digitalWrite(S2,LOW);
-  digitalWrite(S3,HIGH);
-  Serial.print("Blue value= ");
-  DATA = pulseIn(OUT,LOW);
-  delay(10);
-  B = DATA;
-  Serial.print(B); 
-
-  digitalWrite(S2,HIGH);
-  digitalWrite(S3,HIGH);
-  Serial.print("Green value= ");
-  DATA = pulseIn(OUT,LOW);
-  delay(10);
-  G = DATA * 0.65;
-  Serial.println(G); 
-
-  if(R<7 && G<7 && B<7){ LINE = false; }
-  else{ LINE = true;}
+void stop(){
+  M1_SPEED = 0;
+  M2_SPEED = 0;
+  analogWrite(M1_EN,M1_SPEED);
+  analogWrite(M2_EN,M2_SPEED);
+  digitalWrite(M1_CLOCK,LOW);
+  digitalWrite(M2_CLOCK,LOW);
+  digitalWrite(M1_ACLOCK,LOW);
+  digitalWrite(M2_ACLOCK,LOW);
 }
 
 void readIRSensor(){
   IRL_DATA = analogRead(IRL) - AVG_IRL;
   IRR_DATA = analogRead(IRR) - AVG_IRR;
+  IRFL_DATA = analogRead(IRFL) - AVG_IRFL;
+  IRFR_DATA = analogRead(IRFR) - AVG_IRFR;
   if (IRL_DATA - IRL_LDATA > 100){
-    goRight();
-  }
-  else if (IRR_DATA - IRR_LDATA > 100){
     goLeft();
   }
+  else if (IRR_DATA - IRR_LDATA > 100){
+    goRight();
+  }
+  IRL_LDATA = IRL_DATA;
+  IRR_LDATA = IRR_DATA;
+  IRFL_LDATA = IRFL_DATA;
+  IRFR_LDATA = IRFR_DATA;
+  #ifdef DEBUG
+    Serial.print("IRL: ");
+    Serial.print(IRL_DATA);
+    Serial.print(" IRR: ");
+    Serial.print(IRR_DATA);
+    Serial.print(" IRFL: ");
+    Serial.print(IRFL_DATA);
+    Serial.print(" IRFR: ");
+    Serial.println(IRFR_DATA);
+  #endif
 }
 
 void setup()
@@ -131,39 +130,80 @@ void setup()
   Serial.begin(9600);
 
   /* Defining the PINs Array for Input / Output */
-  int outPins[] = {M1_EN, M1_CLOCK, M1_ACLOCK, M2_EN, M2_CLOCK, M2_ACLOCK, IRL_EN, IRR_EN};
-  int inPins[] = {IRL, IRR, S0, S1, S2, S3, OUT};
+  int outPins[] = {M1_EN, M1_CLOCK, M1_ACLOCK, M2_EN, M2_CLOCK, M2_ACLOCK, IR_EN};
+  int inPins[] = {IRL, IRR, IRFL, IRFR, START, STOP};
+  int inPins_size = (sizeof(inPins)/sizeof(inPins[0]));
+  int outPins_size = (sizeof(outPins)/sizeof(outPins[0]));
 
-  for(int i = 0; i < sizeof(outPins); i++){
+  for(int i = 0; i < outPins_size; i++){
     pinMode(outPins[i], OUTPUT);
   }
-  for(int i = 0; i < sizeof(inPins); i++){
+  for(int i = 0; i < inPins_size; i++){
     pinMode(inPins[i], OUTPUT);
   }
-
-  /* Read IR Sensor background values */
-  digitalWrite(IRL_EN, LOW);
-  digitalWrite(IRR_EN, LOW);
+  pinMode(IRL, INPUT);
+  pinMode(IRR, INPUT);
+  pinMode(IRFL, INPUT);
+  pinMode(IRFR, INPUT);
+  
+  digitalWrite(IR_EN, LOW);
 
   for(int i = 0; i < 20; i++){
     AVG_IRL += analogRead(IRL);
     AVG_IRR += analogRead(IRR);
+    AVG_IRFL += analogRead(IRFL);
+    AVG_IRFR += analogRead(IRFR);
+    delay(20);
   }
 
-  AVG_IRL /= 20; AVG_IRR /= 20;
-  IRL_LDATA = AVG_IRL; IRR_LDATA = AVG_IRR;
-  digitalWrite(IRL_EN, HIGH);
-  digitalWrite(IRR_EN, HIGH);
+  AVG_IRL /= 20; AVG_IRR /= 20; AVG_IRFL /= 20; AVG_IRFR /= 20;
+  IRL_LDATA = AVG_IRL; IRR_LDATA = AVG_IRR; IRFL_LDATA = AVG_IRFL; IRFR_LDATA = AVG_IRFR;
+  digitalWrite(IR_EN, HIGH);
 
+  analogWrite(M1_EN,0);
+  analogWrite(M2_EN,0);
+  digitalWrite(M1_CLOCK,LOW);
+  digitalWrite(M2_CLOCK,LOW);
+  digitalWrite(M1_ACLOCK,LOW);
+  digitalWrite(M2_ACLOCK,LOW);
+  
+  startButton.begin();
+  stopButton.begin();
+
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets();
+
+  #ifdef DEBUG
+    Serial.println("Calibration is completed.");
+    Serial.println("IR Sensors Background noise: IRL: " + String(AVG_IRL) + 
+                                               " IRR: " + String(AVG_IRR) + 
+                                               " IRFL: " + String(AVG_IRFL) + 
+                                               " IRFR: " + String(AVG_IRFR));
+  #endif
 }
 
 void loop()
 {
-  analogWrite(M1_EN,240);
-  analogWrite(M2_EN,255);
-  digitalWrite(M1_CLOCK,HIGH);
-  digitalWrite(M2_CLOCK,HIGH);
-  //Serial.println(time);
-  delay(1000);
+  startState = startButton.read();
+  stopState = stopButton.read();
+
+  if(startState != lstartState){
+    if(startState == 1){
+      goStraight();
+      readIRSensor();
+    }
+  }
+
+  if(stopState != lstopState){
+    if(stopState == 1){
+      stop();
+    }
+  }
+
+  lstartState = startState;
+  lstopState = stopState;
+  delay(10);
 }
+
 
